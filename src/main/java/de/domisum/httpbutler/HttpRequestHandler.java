1fe2a23f8ebe50c950b3bc1b3a8d57d007401019
Dominik <dominik.weissenseel@gmx.de>
2018-01-04 17:43:22 +0100

    Added logger to HttpRequestHandler

--- src/main/java/de/domisum/httpbutler/HttpRequestHandler.java
+++ src/main/java/de/domisum/httpbutler/HttpRequestHandler.java
@@ -2,11 +2,17 @@ package de.domisum.httpbutler;
 
 import de.domisum.httpbutler.exceptions.HttpException;
 import de.domisum.lib.auxilium.util.java.annotations.API;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @API
 public abstract class HttpRequestHandler
 {
 
+	protected final Logger logger = LoggerFactory.getLogger(getClass());
+
+
+	// HANDLER
 	protected abstract void handleRequest(HttpRequest request, HttpResponseSender responseSender) throws HttpException;
 
 }
