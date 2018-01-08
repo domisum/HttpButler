c044061db28b2fc9147e423ea8a01f99a9bb69c1
Dominik <dominik.weissenseel@gmx.de>
2018-01-08 16:21:03 +0100

    Added path section parsing

---
+++ src/main/java/de/domisum/httpbutler/exceptions/BadRequestHttpException.java
@@ -0,0 +1,29 @@
+package de.domisum.httpbutler.exceptions;
+
+import de.domisum.httpbutler.HttpResponseSender;
+import de.domisum.lib.auxilium.util.java.annotations.API;
+import io.undertow.util.StatusCodes;
+
+@API
+public class BadRequestHttpException extends HttpException
+{
+
+	// INIT
+	@API public BadRequestHttpException()
+	{
+
+	}
+
+	@API public BadRequestHttpException(String message)
+	{
+		super(message);
+	}
+
+
+	// ERROR
+	@Override public void sendError(HttpResponseSender responseSender)
+	{
+		sendError(responseSender, StatusCodes.BAD_REQUEST, StatusCodes.BAD_REQUEST_STRING);
+	}
+
+}
--- src/main/java/de/domisum/httpbutler/request/HttpRequest.java
+++ src/main/java/de/domisum/httpbutler/request/HttpRequest.java
@@ -1,5 +1,9 @@
 package de.domisum.httpbutler.request;
 
+import de.domisum.httpbutler.exceptions.BadRequestHttpException;
+import de.domisum.lib.auxilium.util.PHR;
+import de.domisum.lib.auxilium.util.StringUtil;
+import de.domisum.lib.auxilium.util.java.annotations.API;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
@@ -15,5 +19,33 @@ public class HttpRequest
 	@Getter private final HttpMethod method;
 	@Getter private final String path;
 	@Getter private final Map<String, List<String>> queryParameters;
+
+
+	@API public String getPathSegment(int segmentIndex) throws BadRequestHttpException
+	{
+		String[] pathSplit = path.split(StringUtil.escapeStringForRegex("/"));
+
+		// path starts with slash, but first argument after slash should be regarded as 0
+		int splitIndex = segmentIndex+1;
+		if(splitIndex >= pathSplit.length)
+			throw new BadRequestHttpException("request did not contain path segment on index "+segmentIndex);
+
+		return pathSplit[splitIndex];
+	}
+
+	@API public int getPathSegmentAsInt(int segementIndex) throws BadRequestHttpException
+	{
+		String pathSegmentString = getPathSegment(segementIndex);
+
+		try
+		{
+			return Integer.parseInt(pathSegmentString);
+		}
+		catch(NumberFormatException ignored)
+		{
+			throw new BadRequestHttpException(PHR.r("path segment on index {} supposed to be integer, was {}",
+					pathSegmentString));
+		}
+	}
 
 }
