707f3911480583730b01963394e340d521fd086b
Dominik <dominik.weissenseel@gmx.de>
2018-07-03 19:37:04 +0200

    Added forbidden http exception

---
+++ src/main/java/de/domisum/httpbutler/exceptions/ForbiddenHttpException.java
@@ -0,0 +1,26 @@
+package de.domisum.httpbutler.exceptions;
+
+import de.domisum.httpbutler.HttpResponseSender;
+import de.domisum.lib.auxilium.util.java.annotations.API;
+import io.undertow.util.StatusCodes;
+import lombok.NoArgsConstructor;
+
+@API
+@NoArgsConstructor
+public class ForbiddenHttpException extends HttpException
+{
+
+	// INIT
+	@API public ForbiddenHttpException(String message)
+	{
+		super(message);
+	}
+
+
+	// ERROR
+	@Override public void sendError(HttpResponseSender responseSender)
+	{
+		sendError(responseSender, StatusCodes.FORBIDDEN, StatusCodes.FORBIDDEN_STRING);
+	}
+
+}
