32639bbf7a45761febcf92edcf8d9e0f37b70a12
domisum <dominik.weissenseel@gmail.com>
2019-07-19 20:00:13 +0200

    Added bad gateway exception

---
+++ src/main/java/de/domisum/httpbutler/exceptions/BadGatewayHttpException.java
@@ -0,0 +1,32 @@
+package de.domisum.httpbutler.exceptions;
+
+import de.domisum.httpbutler.HttpResponseSender;
+import de.domisum.lib.auxilium.util.java.annotations.API;
+import io.undertow.util.StatusCodes;
+
+@API
+public class BadGatewayHttpException extends HttpException
+{
+
+	// INIT
+	@API
+	public BadGatewayHttpException()
+	{
+
+	}
+
+	@API
+	public BadGatewayHttpException(String message)
+	{
+		super(message);
+	}
+
+
+	// ERROR
+	@Override
+	public void sendError(HttpResponseSender responseSender)
+	{
+		sendError(responseSender, StatusCodes.BAD_GATEWAY, StatusCodes.BAD_GATEWAY_STRING);
+	}
+
+}
