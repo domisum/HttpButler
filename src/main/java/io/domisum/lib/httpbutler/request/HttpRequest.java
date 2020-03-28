1929a1df9e198c20e6592cfb78430a0fc89ef558
domisum <dominik.weissenseel@gmail.com>
2020-03-28 20:56:43 +0100

    Improved request methods

--- src/main/java/io/domisum/lib/httpbutler/request/HttpRequest.java
+++ src/main/java/io/domisum/lib/httpbutler/request/HttpRequest.java
@@ -16,7 +16,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.function.Function;
 
 public class HttpRequest
@@ -26,7 +25,7 @@ public class HttpRequest
 	@Getter
 	private final HttpMethod method;
 	@Getter
-	private final String path;
+	private final String path; // no slash at end or beginning
 	
 	private final Map<String,List<String>> queryParameters; // keys lowercase
 	private final Map<String,List<String>> headers; // keys lowercase
@@ -93,29 +92,25 @@ public class HttpRequest
 	{
 		String[] pathSplit = path.split(StringUtil.escapeStringForRegex("/"));
 		
-		// path starts with slash, but first argument after slash should be regarded as 0
-		int splitIndex = segmentIndex+1;
-		if(splitIndex >= pathSplit.length)
+		if(segmentIndex >= pathSplit.length)
 			throw new BadRequestHttpException("request did not contain path segment on index "+segmentIndex);
 		
-		return pathSplit[splitIndex];
+		return pathSplit[segmentIndex];
 	}
 	
 	@API
-	public int getPathSegmentAsInt(int segementIndex)
+	public <T> T parsePathSegment(int segmentIndex, Function<String,T> parser)
 			throws BadRequestHttpException
 	{
-		String pathSegmentString = getPathSegment(segementIndex);
-		
+		String pathSegmentString = getPathSegment(segmentIndex);
 		try
 		{
-			return Integer.parseInt(pathSegmentString);
+			return parser.apply(pathSegmentString);
 		}
-		catch(NumberFormatException ignored)
+		catch(RuntimeException e)
 		{
-			throw new BadRequestHttpException(PHR.r("path segment on index {} supposed to be integer, was {}",
-					pathSegmentString
-			));
+			String error = PHR.r("invalid value for path segment at index {}, given: '{}', problem: {}", segmentIndex, pathSegmentString, e.getMessage());
+			throw new BadRequestHttpException(error);
 		}
 	}
 	
@@ -166,7 +161,7 @@ public class HttpRequest
 	
 	// HEADERS
 	@API
-	public List<String> getHeaderValuesOrException(String key)
+	public List<String> getHeaderValues(String key)
 			throws BadRequestHttpException
 	{
 		if(!headers.containsKey(key.toLowerCase()))
@@ -176,38 +171,14 @@ public class HttpRequest
 	}
 	
 	@API
-	public String getHeaderValueOrException(String key)
+	public String getHeaderValue(String key)
 			throws BadRequestHttpException
 	{
-		var headerValues = getHeaderValuesOrException(key);
+		var headerValues = getHeaderValues(key);
 		if(headerValues.size() > 1)
 			throw new BadRequestHttpException(PHR.r("request contained multiple values for header '{}', must be one", key));
 		
 		return Iterables.getOnlyElement(headerValues);
-	}
-	
-	@API
-	public Optional<List<String>> getHeaderValues(String key)
-	{
-		if(!headers.containsKey(key.toLowerCase()))
-			return Optional.empty();
-		
-		return Optional.of(headers.get(key.toLowerCase()));
-	}
-	
-	@API
-	public Optional<String> getHeaderValue(String key)
-			throws BadRequestHttpException
-	{
-		var headerValuesOptional = getHeaderValues(key);
-		if(headerValuesOptional.isEmpty())
-			return Optional.empty();
-		
-		var headerValues = headerValuesOptional.get();
-		if(headerValues.size() > 1)
-			throw new BadRequestHttpException(PHR.r("request contained multiple values for header '{}', must be one", key));
-		
-		return Optional.of(headerValues.get(0));
 	}
 	
 	
