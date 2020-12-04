dec9c7d580d0e5ef06487edcff6598b131711da9
domisum <me@domisum.io>
2020-12-04 12:45:09 +0100

    Add customizable contentType to HttpResponseStream

--- src/main/java/io/domisum/lib/httpbutler/responses/HttpResponseStream.java
+++ src/main/java/io/domisum/lib/httpbutler/responses/HttpResponseStream.java
@@ -1,28 +1,29 @@
 package io.domisum.lib.httpbutler.responses;
 
 import io.domisum.lib.auxiliumlib.annotations.API;
-import io.domisum.lib.auxiliumlib.util.ValidationUtil;
 import io.domisum.lib.httpbutler.HttpResponse;
 import io.undertow.server.HttpServerExchange;
 import io.undertow.util.Headers;
+import lombok.RequiredArgsConstructor;
 
 import java.io.IOException;
 import java.io.InputStream;
 
 @API
+@RequiredArgsConstructor
 public class HttpResponseStream
 	extends HttpResponse
 {
 	
 	// ATTRIBUTES
+	private final String contentType;
 	private final InputStream stream;
 	
 	
 	// INIT
 	public HttpResponseStream(InputStream stream)
 	{
-		ValidationUtil.notNull(stream, "stream");
-		this.stream = stream;
+		this("application/octet-stream", stream);
 	}
 	
 	
@@ -31,7 +32,7 @@ public class HttpResponseStream
 	protected void sendSpecific(HttpServerExchange httpServerExchange)
 		throws IOException
 	{
-		httpServerExchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "application/octet-stream");
+		httpServerExchange.getResponseHeaders().put(Headers.CONTENT_TYPE, contentType);
 		stream.transferTo(httpServerExchange.getOutputStream());
 	}
 	
