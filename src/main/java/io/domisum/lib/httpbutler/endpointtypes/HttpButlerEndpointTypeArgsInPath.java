f5c2d10595d25d9ac7b9a42c0cb1aee596d64df6
domisum <me@domisum.io>
2025-12-12 08:55:07 +0100

    Add HttpButlerEndpointTypeArgsInPath.arg

--- src/main/java/io/domisum/lib/httpbutler/endpointtypes/HttpButlerEndpointTypeArgsInPath.java
+++ src/main/java/io/domisum/lib/httpbutler/endpointtypes/HttpButlerEndpointTypeArgsInPath.java
@@ -1,8 +1,10 @@
 package io.domisum.lib.httpbutler.endpointtypes;
 
+import io.domisum.lib.auxiliumlib.PHR;
 import io.domisum.lib.auxiliumlib.annotations.API;
 import io.domisum.lib.auxiliumlib.util.StringUtil;
 import io.domisum.lib.httpbutler.HttpButlerEndpoint;
+import io.domisum.lib.httpbutler.exceptions.HttpBadRequest;
 import io.domisum.lib.httpbutler.request.HttpMethod;
 import io.domisum.lib.httpbutler.request.HttpRequest;
 
@@ -34,13 +36,30 @@ public abstract class HttpButlerEndpointTypeArgsInPath
 		
 		String endpointPathWithPlaceholders = HttpRequest.cleanUpPath(PATH_WITH_PLACEHOLDERS()).toLowerCase();
 		String pathWithPlaceholdersEscaped = StringUtil.escapeStringForRegex(endpointPathWithPlaceholders);
-		String pathRegex = "^"+pathWithPlaceholdersEscaped.replace(PLACEHOLDER, PARAMETER_REGEX)+"$";
+		String pathRegex = "^" + pathWithPlaceholdersEscaped.replace(PLACEHOLDER, PARAMETER_REGEX) + "$";
 		
 		boolean matches = request.getPath().toLowerCase().matches(pathRegex);
 		if(!matches)
 			return DOES_NOT_ACCEPT;
 		
 		return endpointPathWithPlaceholders.length();
+	}
+	
+	
+	// UTIL
+	protected String arg(int index, HttpRequest request)
+		throws HttpBadRequest
+	{
+		var segments = StringUtil.splitByLiteral(PATH_WITH_PLACEHOLDERS(), "/");
+		int phi = 0;
+		for(int i = 0; i < segments.size(); i++)
+			if(segments.get(i).equals(PLACEHOLDER))
+			{
+				if(phi == index)
+					return request.getPathSegment(i);
+				phi++;
+			}
+		throw new IllegalArgumentException(PHR.r("Path only has {} placeholders, but requested index {}", phi, index));
 	}
 	
 }
